638065d1ac4cc7d167fc484525b77385fe283e66
marieroca <marieroca@172.17.47.208>
2018-11-24 11:18:55 +0100

    Le format date marche enfin putain

--- src/donnes/date/DateConvention.java
+++ src/donnes/date/DateConvention.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class DateConvention implements Serializable{
     private Date date;
     final static SimpleDateFormat FORMAT = new SimpleDateFormat("dd/MM/yyyy");
-    static final long DUREE_UN_JOUR = 24 * 60 * 60 * 1000;
+    //static final long DUREE_UN_JOUR = 24 * 60 * 60 * 1000;
     /*//Si on veut prendre en compte différents formats de dates
         String s = "";
         if (dateEntree.contains("/"))
@@ -64,20 +64,22 @@ public class DateConvention implements Serializable{
      *          Date de fin
      * @return le nombre de jours entre deux dates
      */
-    public static int getNbJours(Date dateDeb, Date dateFin){
+    /*public static int getNbJours(Date dateDeb, Date dateFin){
         long diff = Math.abs(dateDeb.getTime() - dateFin.getTime());
         long nbJours = (long)diff/DUREE_UN_JOUR;
         return (int)nbJours;
-    }
+    }*/
     
     public static int nbMois(Date dateDeb, Date dateFin){
-        if(dateFin.getDay() >= dateDeb.getDay()){
+        int jDeb = (new DateConvention(dateDeb)).getJour();
+        int jFin = (new DateConvention(dateFin)).getJour();
+        if(jFin >= jDeb){
             if(dateFin.getYear() == dateDeb.getYear())
                 return dateFin.getMonth()- dateDeb.getMonth();
             else if(dateFin.getYear() > dateDeb.getYear())
                 return (dateFin.getYear() - dateDeb.getYear())*12 - dateDeb.getMonth() + dateDeb.getMonth();
         }
-        else if(dateFin.getDay() < dateDeb.getDay()){
+        else if(jFin < jDeb){
             if(dateFin.getYear() == dateDeb.getYear())
                 return dateFin.getMonth() - dateDeb.getMonth() - 1;
             else if(dateFin.getYear() > dateDeb.getYear())
@@ -87,8 +89,12 @@ public class DateConvention implements Serializable{
     }
     
     public static int nbJours(Date dateDeb, Date dateFin){
-        if(dateFin.getDay() >= dateDeb.getDay())
-            return dateFin.getDay() - dateDeb.getDay();
+        int jDeb = (new DateConvention(dateDeb)).getJour();
+        int jFin = (new DateConvention(dateFin)).getJour();
+        if(jFin >= jDeb){
+            return jFin - jDeb;
+        }
+            
         else
             switch(dateFin.getMonth()){
                 case 2 :
@@ -96,11 +102,11 @@ public class DateConvention implements Serializable{
                 case 6 :
                 case 9 :
                 case 11 :
-                    return 31 - dateDeb.getDay() + dateFin.getDay();
+                    return 31 - jDeb + jFin;
                 case 3 :
-                    return 29 - dateDeb.getDay() + dateFin.getDay();
+                    return 29 - jDeb + jFin;
                 default :
-                    return 30 - dateDeb.getDay() + dateFin.getDay();
+                    return 30 - jDeb + jFin;
             }
     }
     
@@ -116,5 +122,18 @@ public class DateConvention implements Serializable{
     public Date getDate(){
         return date;
     } 
+    
+    /**
+     * 
+     */
+    public int getJour(){
+        String j = FORMAT.format(date).split("/")[0];
+        //int jour = ;
+        if(j.toCharArray()[0] == '0')
+            return Integer.parseInt(j.toCharArray()[1]+"");
+        //String jour = ;
+        //System.out.println(jour);
+        return Integer.parseInt(j);
+    }
     
 }
